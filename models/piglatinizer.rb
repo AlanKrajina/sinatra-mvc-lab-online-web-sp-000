a9a619166c55abfa2ac9dbbbed46da01ed9ddfc2
Alan Krajina <alankrajina88@gmail.com>
2019-07-13 14:01:02 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -18,4 +18,4 @@ end
 
 #        word[0] =~ /[aeiou]/ ? word.gsub(/([aeiou])(\w+)*/, '\1\2way') : word.gsub(/([^aeiou])(\w+)*/, '\2\1ay')
 
-oldfake.split[0] =~ /[aeiou]/ ? fake.gsub(/([aeiou])(\w+)*/, '\1\2way') : fake.gsub(/([^aeiou])(\w+)*/, '\2\1ay')
+oldfake.split[0] =~ /[aeiou]/ ? oldfake.split[0].gsub(/([aeiou])(\w+)*/, '\1\2way') : oldfake.split[0].gsub(/([^aeiou])(\w+)*/, '\2\1ay')
