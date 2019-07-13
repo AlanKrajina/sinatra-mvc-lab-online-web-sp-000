8c63ea1158d255df1599a2b2712bbe3150a7e0fb
Alan Krajina <alankrajina88@gmail.com>
2019-07-13 13:59:58 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -17,3 +17,5 @@ end
 
 
 #        word[0] =~ /[aeiou]/ ? word.gsub(/([aeiou])(\w+)*/, '\1\2way') : word.gsub(/([^aeiou])(\w+)*/, '\2\1ay')
+
+oldfake.split[0] =~ /[aeiou]/ ? fake.gsub(/([aeiou])(\w+)*/, '\1\2way') : fake.gsub(/([^aeiou])(\w+)*/, '\2\1ay')
