7b68670a8c79654955065b0fa83993a15519a030
Alan Krajina <alankrajina88@gmail.com>
2019-07-13 14:26:30 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -13,7 +13,8 @@ word1= word.split[0][0] =~ /[aeiou]/ ? word.split[0].gsub(/([aeiou])(\w+)*/, '\1
 array << word1
 word2=word.split[1][0] =~ /[aeiou]/ ? word.split[1].gsub(/([aeiou])(\w+)*/, '\1\2way') : word.split[1].gsub(/([^aeiou])(\w+)*/, '\2\1ay')
 
- word2
+array <<  word2
+array
   end
 end
 end
