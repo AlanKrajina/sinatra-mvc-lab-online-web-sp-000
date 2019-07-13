d9a468d17e68fd08579f234faba18a615f6a7df0
Alan Krajina <alankrajina88@gmail.com>
2019-07-13 13:51:43 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,6 +3,9 @@ class PigLatinizer
 
   def piglatinize(word)
 
+word[0] =~ /[aeiou]/ ? word.gsub(/([aeiou])(\w+)*/, '\1\2way') : word.gsub(/([^aeiou])(\w+)*/, '\2\1ay')
+
+word[1] =~ /[aeiou]/ ? word.gsub(/([aeiou])(\w+)*/, '\1\2way') : word.gsub(/([^aeiou])(\w+)*/, '\2\1ay')
 
   end
 
