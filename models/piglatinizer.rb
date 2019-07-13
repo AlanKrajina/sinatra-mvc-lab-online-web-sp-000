a2ed49e8628ee2914c862649fb57a7b5d8c037a3
Alan Krajina <alankrajina88@gmail.com>
2019-07-13 14:25:51 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,6 +2,7 @@ class PigLatinizer
 
 
   def piglatinize(word)
+    array =[]
     word="noodle soup"
     if word.split.length == 1
       word[0] =~ /[aeiou]/ ? word.gsub(/([aeiou])(\w+)*/, '\1\2way') : word.gsub(/([^aeiou])(\w+)*/, '\2\1ay')
@@ -12,7 +13,7 @@ word1= word.split[0][0] =~ /[aeiou]/ ? word.split[0].gsub(/([aeiou])(\w+)*/, '\1
 
 word2=word.split[1][0] =~ /[aeiou]/ ? word.split[1].gsub(/([aeiou])(\w+)*/, '\1\2way') : word.split[1].gsub(/([^aeiou])(\w+)*/, '\2\1ay')
 
-word1
+array << word1, word2
   end
 end
 end
