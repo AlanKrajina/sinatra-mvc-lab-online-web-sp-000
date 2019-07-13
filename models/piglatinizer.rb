b800ba6d87070230321e684d3556e2e897547e94
Alan Krajina <alankrajina88@gmail.com>
2019-07-13 14:11:41 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,14 +2,17 @@ class PigLatinizer
 
 
   def piglatinize(word)
-    if word == 
+    if word.is_a? String 
+      word[0] =~ /[aeiou]/ ? word.gsub(/([aeiou])(\w+)*/, '\1\2way') : word.gsub(/([^aeiou])(\w+)*/, '\2\1ay')
+      
+    else
 word.split[0][0] =~ /[aeiou]/ ? word.split[0].gsub(/([aeiou])(\w+)*/, '\1\2way') : word.split[0].gsub(/([^aeiou])(\w+)*/, '\2\1ay')
 
 word.split[1][0] =~ /[aeiou]/ ? word.split[1].gsub(/([aeiou])(\w+)*/, '\1\2way') : word.split[1].gsub(/([^aeiou])(\w+)*/, '\2\1ay')
 
   end
 
-
+end
 end
 
 
